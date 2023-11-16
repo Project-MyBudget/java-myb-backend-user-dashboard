72250728de7b05cfc1fcae36ea267350a5d3a56e
Marcos V Ramos <viniciusramoscaldas@gmail.com>
2023-11-16 00:39:27 -0300

    Creating UserBudget Amount

--- src/main/java/br/com/mybudget/userdashboard/service/UserChartService.java
+++ src/main/java/br/com/mybudget/userdashboard/service/UserChartService.java
@@ -1,9 +1,13 @@
 package br.com.mybudget.userdashboard.service;
 
 import br.com.mybudget.userdashboard.model.dto.UserChartEnvelopeDTO;
+
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserChartService {
     UserChartEnvelopeDTO  getUserDebts(Long idUser);
+    BigDecimal getTotalBudgetAmount(Long idUser);
 }
